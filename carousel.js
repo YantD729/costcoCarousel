f91e1a0592f9a08a8285c8d071be12f1434065b4
Yan t D <yantingd@andrew.cmu.edu>
2023-06-06 18:55:16 +0800

    a version of autoplay

--- carousel.js
+++ carousel.js
@@ -3,6 +3,9 @@ console.log(buttons);
 const slides = document.querySelectorAll(".slide");
 const dots = document.querySelectorAll(".dot-icon");
 const numOfSlides = slides.length;
+const interval = 2000;
+var autoPlayInterval, autoPlayer;
+
 
 let activeIndex = 0;
 let currSlide = null;
@@ -16,8 +19,36 @@ buttons.forEach(button => {
         if (activeIndex >= slides.length) activeIndex = 0;
 
         slides[activeIndex].dataset.active = true;
-        console.log(slides[activeIndex].dataset.active);
         delete currSlide.dataset.active;
-        console.log(currSlide);
     })
-})
+});
+
+autoPlayer = () => {
+    autoPlayInterval = setInterval(function() {
+        document.querySelector(".next-btn").click();
+        }, interval);
+};
+
+autoPlayer();
+
+//stop the image play on mouseover
+slides.addEventListener("mousrover", () => {
+    clearInterval(autoPlayInterval);
+});
+
+buttons.addEventListener("mousrover", () => {
+    clearInterval(autoPlayInterval);
+});
+
+ //start the image slider autoplay again on mouseout
+ slides.addEventListener("mouseout", () => {
+    autoPlayer();
+  });
+
+  buttons.addEventListener("mouseout", () => {
+    autoPlayer();
+  });
+
+
+
+
