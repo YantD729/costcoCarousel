cf394fa583f8c7e80d123e7cbf569fad53591981
Yan t D <yantingd@andrew.cmu.edu>
2023-06-06 14:20:00 +0800

    a version that slides worked

--- carousel.js
+++ carousel.js
@@ -1,2 +1,23 @@
-import html from 'index.html';
-import css from 'css/styles.css';
+const buttons = document.querySelectorAll("[data-carousel-button]");
+console.log(buttons);
+const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".dot-icon");
+const numOfSlides = slides.length;
+
+let activeIndex = 0;
+let currSlide = null;
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        currSlide = slides[activeIndex];
+        const offset = (button.dataset.carouselButton === 'next') ? 1 : -1;
+        activeIndex += offset;
+        if (activeIndex < 0) activeIndex  = slides.length - 1;
+        if (activeIndex >= slides.length) activeIndex = 0;
+
+        slides[activeIndex].dataset.active = true;
+        console.log(slides[activeIndex].dataset.active);
+        delete currSlide.dataset.active;
+        console.log(currSlide);
+    })
+})
