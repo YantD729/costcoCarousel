1ca7b9256426238e156b734c79f987fe5abfe0ed
Yan t D <yantingd@andrew.cmu.edu>
2023-06-09 10:58:01 +0800

    fix carousels button

--- carousel.js
+++ carousel.js
@@ -1,88 +1,109 @@
-const buttons = document.querySelectorAll("[data-carousel-button]");
-console.log(buttons);
-const slides = document.querySelectorAll(".slide");
-const dots = document.querySelectorAll(".dot-icon");
-const numOfSlides = slides.length;
-const interval = 2000;
-const toggleBtn = document.querySelector(".playToggle");
-var autoPlayInterval, autoPlayer;
+const buttons = document.querySelectorAll("[data-carousel-button]")
+console.log(buttons)
+const slides = document.querySelectorAll(".slide")
+const dots = document.querySelectorAll(".dot-icon")
+const numOfSlides = slides.length
+const interval = 2000
+const toggleBtn = document.querySelector(".playToggle")
+let autoPlayInterval, autoPlayer
 
 
-let activeIndex = 0;
-let currSlide = null;
-let isPaused = false;
+let activeIndex = 0
+let currSlide = null
+let isPaused = false
+let isTogglePaused = false
 
 buttons.forEach(button => {
     button.addEventListener("click", () => {
-        currSlide = slides[activeIndex];
-        currDot = dots[activeIndex];
-        const offset = (button.dataset.carouselButton === 'next') ? 1 : -1;
-        activeIndex += offset;
-        if (activeIndex < 0) activeIndex  = slides.length - 1;
-        if (activeIndex >= slides.length) activeIndex = 0;
+        currSlide = slides[activeIndex]
+        currDot = dots[activeIndex]
+        const offset = (button.dataset.carouselButton === 'next') ? 1 : -1
+        activeIndex += offset
+        if (activeIndex < 0) activeIndex  = slides.length - 1
+        if (activeIndex >= slides.length) activeIndex = 0
 
-        slides[activeIndex].dataset.active = true;
-        dots[activeIndex].dataset.active = true;
-        delete currSlide.dataset.active;
-        delete currDot.dataset.active;
+        slides[activeIndex].dataset.active = true
+        dots[activeIndex].dataset.active = true
+        delete currSlide.dataset.active
+        delete currDot.dataset.active
     })
-});
+})
 
 autoPlayer = () => {
     autoPlayInterval = setInterval(function() {
-        document.querySelector(".next-btn").click();
-        }, interval);
-};
+        document.querySelector(".next-btn").click()
+        }, interval)
+}
 
-autoPlayer();
+autoPlayer()
 
 //stop the image play on mouseover
 slides.forEach(slide => {
     slide.addEventListener("mouseover", () => {
-        clearInterval(autoPlayInterval);
-    });
+        if (!isTogglePaused) {
+            if (!isPaused) {
+                clearInterval(autoPlayInterval) 
+                isPaused = true
+            }
+        }
+    })
 
     slide.addEventListener("mouseout", () => {
-        autoPlayer();
-    });
-});
+        if (!isTogglePaused) {
+            if (isPaused) {
+                autoPlayer() 
+                isPaused = false
+            }
+        }
+    })
+})
 
 buttons.forEach(button => {
     button.addEventListener("mouseover", () => {
-        clearInterval(autoPlayInterval);
-    });
+        if (!isTogglePaused) {
+            if (!isPaused) {
+                clearInterval(autoPlayInterval) 
+                isPaused = true
+            }
+        }
+    })
 
     button.addEventListener("mouseout", () => {
-        autoPlayer();
-    });
-});
+        if (!isTogglePaused) {
+            if (isPaused) {
+                autoPlayer() 
+                isPaused = false
+            }
+        }
+    })
+})
 
 //set the play toggle
 toggleBtn.addEventListener("click", () => {
-    if (isPaused) {
-        autoPlayer();
-        isPaused = false;
-        toggleBtn.textContent = '❚❚';
+    if (isTogglePaused) {
+        autoPlayer()
+        isTogglePaused = false
+        toggleBtn.textContent = '❚❚'
     }
-    else {
-        clearInterval(autoPlayInterval);
-        isPaused = true;
-        toggleBtn.textContent = '>';
+    else if (!isTogglePaused) {
+        clearInterval(autoPlayInterval)
+        isTogglePaused = true
+        toggleBtn.textContent = '>'
     }
-});
+})
 
 //set the dot button
 dots.forEach((dot, index) => {
     dot.addEventListener("click", () => {
-        currSlide = slides[activeIndex];
-        currDot = dots[activeIndex];
-        slides[index].dataset.active = true;
-        dots[index].dataset.active = true;
-        activeIndex = index;
-        delete currSlide.dataset.active;
-        delete currDot.dataset.active;
-    });
-});
+        currSlide = slides[activeIndex]
+        currDot = dots[activeIndex]
+        slides[index].dataset.active = true
+        dots[index].dataset.active = true
+        activeIndex = index
+        delete currSlide.dataset.active
+        delete currDot.dataset.active
+    })
+})
 
 
 
