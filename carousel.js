0bcc7785d4be962158421aa8c6331c27fded86b6
Yan t D <yantingd@andrew.cmu.edu>
2023-06-10 22:54:03 +0800

    works without dot pics

--- carousel.js
+++ carousel.js
@@ -1,17 +1,70 @@
 const buttons = document.querySelectorAll("[data-carousel-button]")
 console.log(buttons)
-const slides = document.querySelectorAll(".slide")
-const dots = document.querySelectorAll(".dot-icon")
-const numOfSlides = slides.length
 const interval = 2000
-const toggleBtn = document.querySelector(".playToggle")
+// const toggleBtn = document.querySelector(".playToggle")
+let slidesContainer = document.querySelector("#slides")
+
+let dotsContainer = document.querySelector("#dots")
 let autoPlayInterval, autoPlayer
 
+const slidesData = [
+    {url: 'https://mobilecontent.costco.com/live/resource/img/23w10152/d-23w10152-fq-june-mailer.jpg'},
+    {url: 'https://media-cdn.costco.com/www3-media/?libBID=3843303'},
+    {url: 'https://media-cdn.costco.com/www3-media/?libBID=3862617'},
+    {url: 'https://mobilecontent.costco.com/live/resource/img/5-17-23-MVM/d-23w09134-may-mvm-fq.jpg'},
+    {url: 'https://media-cdn.costco.com/www3-media/?libBID=3848466'},
+    {url: 'https://media-cdn.costco.com/www3-media/?libBID=3849522'},
+    {url: 'https://media-cdn.costco.com/www3-media/?libBID=3848209'},
+    {url: 'https://media-cdn.costco.com/www3-media/?libBID=3839053'},
+    {url: 'https://media-cdn.costco.com/www3-media/?libBID=3848498'},
+    {url: 'https://media-cdn.costco.com/www3-media/?libBID=3736261'},
+]
 
+const numOfslidesData = slidesData.length
 let activeIndex = 0
 let currSlide = null
 let isPaused = false
 let isTogglePaused = false
+
+//add slidesData to slidesContainer
+
+slidesData.forEach(function(slideData, index) {
+    let slideElem = document.createElement('div')
+    slideElem.className = 'slide'
+
+    let imgElem = document.createElement('img')
+    imgElem.src = slideData.url
+
+    if (index === 0) slideElem.setAttribute('data-active', '')
+
+    slideElem.appendChild(imgElem)
+    slidesContainer.appendChild(slideElem)
+})
+
+const slides = document.querySelectorAll('.slide')
+
+//add dots
+for (let i = 0; i < numOfslidesData; i++) {
+    let dotBtn = document.createElement("div")
+    let button = document.createElement("button")
+    button.className = "play-icon dot-icon"
+    button.type = "button"
+    dotBtn.className = "dot-box"
+    dotBtn.appendChild(button)
+    if (i === 0) dotBtn.setAttribute('data-active', '')
+    dotsContainer.appendChild(dotBtn)
+}
+
+let toggleBox = document.createElement("div")
+toggleBox.className = "dot-box"
+let toggleBtn = document.createElement("button")
+toggleBtn.className = "play-icon playToggle"
+toggleBtn.type = "button"
+toggleBtn.textContent = '❚❚'
+toggleBox.appendChild(toggleBtn);
+dotsContainer.appendChild(toggleBox)
+
+const dots = document.querySelectorAll(".play-icon")
 
 buttons.forEach(button => {
     button.addEventListener("click", () => {
@@ -95,9 +148,9 @@ toggleBtn.addEventListener("click", () => {
 //set the dot button
 dots.forEach((dot, index) => {
     dot.addEventListener("click", () => {
-        currSlide = slides[activeIndex]
+        currSlide = slidesData[activeIndex]
         currDot = dots[activeIndex]
-        slides[index].dataset.active = true
+        slidesData[index].dataset.active = true
         dots[index].dataset.active = true
         activeIndex = index
         delete currSlide.dataset.active
